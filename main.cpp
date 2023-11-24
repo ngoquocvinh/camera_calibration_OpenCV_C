1caa2e68a477e1416ec69dc4b522df3997e8b1ea
Ngo Quoc Vinh <51219831+ngoquocvinh@users.noreply.github.com>
2023-11-24 23:19:11 +0700

    thêm chú thích

---
+++ main.cpp
@@ -0,0 +1,315 @@
+/*
+  This program captures video from a camera, detects a chessboard pattern, calibrates the camera,
+  undistorts an image, and then processes a video stream to find and measure distances between objects.
+
+  How to use
+  1. prepare chessboard with 10x7 squares. each square is 22x22mm
+  2. install Droidcam on iphone and run it to get the ip like: http://192.168.1.6:4747
+  3. the iphone camera and computer must be a same network
+  4. fixed camera positon and use chessboard for calibrication
+  5. put the chessboard in the camera view and press spacebar key to capture a sample.
+  6. continue do it 12 times to gather 12 sample for the internal and external calibrication matrix calculation
+  7. press the spacebar for calculate two farthest distance, compare with your real manually measure in milimeter
+  8. press the spacebar and put two blue object to detect distance between them. Rotate one of them for visualling
+  9. the distance may not correct, please check your calibration methods
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+using namespace cv;
+
+// Function to compute the distance in pixels between corners of a chessboard pattern
+double computeDistancePixels(const vector<Point2f> &corners);
+
+// Function to compare contour areas for sorting
+bool compareContourAreas(vector<Point> contour1, vector<Point> contour2);
+
+// Function to compute the scale factor based on the chessboard pattern
+double computeScaleFactor(const Size &patternSize, const float squareSize, const vector<Point2f> &corners);
+
+// Function to compute the distance in real-world units between two points
+double computeDistanceRealUnits(const Point2f &p1, const Point2f &p2, double scaleFactor);
+
+// Function to process the video stream and draw boundaries around detected objects
+void process_and_draw_boundaries(Mat &frame, Mat &threshold_output, vector<Point2f> &centroids,
+                                 const Mat &cameraMatrix, const vector<double> &distCoeffs);
+
+/*
+  This main function captures video from a camera, performs chessboard pattern calibration,
+  calibrates the camera, undistorts an image, and then processes a video stream to find and measure
+  distances between objects.
+*/
+int main()
+{
+
+    // Images and parameters for chessboard pattern calibration
+    Mat image, otsu;
+    Size patternSize(9, 6);
+    int samples = 12;
+    int collected_samples = 0;
+
+    // 3D object points for chessboard pattern calibration
+    vector<Point3f> objectPoints;
+    float squareSize = 22.0;
+
+    // Generate 3D object points based on the chessboard pattern
+    for (int i = 0; i < patternSize.height; i++)
+    {
+        for (int j = 0; j < patternSize.width; j++)
+        {
+            objectPoints.push_back(Point3f(j * squareSize, i * squareSize, 0));
+        }
+    }
+
+    // Vectors to store object points and image points for calibration
+    vector<vector<Point3f>> objectPointsArray;
+    vector<vector<Point2f>> imagePointsArray;
+
+    const string videoStreamAddress = "http://192.168.1.6:4747/video";
+    VideoCapture vcap;
+    // Open video stream
+    if (!vcap.open(videoStreamAddress))
+    {
+        cerr << "Error opening video stream or file" << endl;
+        return -1;
+    }
+
+    // Create a window for displaying the chessboard pattern
+    namedWindow("Chessboard", WINDOW_NORMAL);
+
+    // Collect chessboard pattern samples
+    while (collected_samples < samples)
+    {
+        vcap.read(image);
+        imshow("Chessboard", image);
+
+        int key = waitKey(0);
+
+        if (key == 27)
+        {
+            break;
+        }
+
+        if (key == ' ')
+        {
+            vector<Point2f> corners;
+            bool found = findChessboardCorners(image, patternSize, corners);
+
+            if (found)
+            {
+                drawChessboardCorners(image, patternSize, corners, found);
+                imshow("Chessboard", image);
+                collected_samples++;
+
+                imagePointsArray.push_back(corners);
+                objectPointsArray.push_back(objectPoints);
+
+                cout << "Collected samples: " << collected_samples << endl;
+            }
+        }
+    }
+
+    // Calibrate the camera
+    Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+    vector<double> distCoeffs;
+    vector<Mat> rvecs, tvecs;
+
+    calibrateCamera(objectPointsArray, imagePointsArray, image.size(), cameraMatrix, distCoeffs, rvecs, tvecs);
+
+    // Display camera calibration results
+    cout << "Camera matrix: " << format(cameraMatrix, Formatter::FMT_CSV) << endl;
+
+    Mat distCoeffsMat = Mat(distCoeffs);
+    cout << "Distortion coefficients: " << format(distCoeffsMat, Formatter::FMT_CSV) << endl;
+
+    // Undistort the captured image
+    Mat undistortedImage;
+    undistort(image, undistortedImage, cameraMatrix, distCoeffs);
+
+    // Compute and display distance in pixels and real-world units
+    double distancePixels = computeDistancePixels(imagePointsArray[0]);
+    double distanceMM = distancePixels * squareSize / norm(imagePointsArray[0][0] - imagePointsArray[0][1]);
+
+    putText(undistortedImage, "Distance: " + to_string(distancePixels) + " pixels", Point(10, 30),
+            FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
+    putText(undistortedImage, "Distance: " + to_string(distanceMM) + " mm", Point(10, 60),
+            FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 0, 255), 2);
+
+    // Find and mark the two farthest points in the undistorted image
+    Point2f farthestPoint1, farthestPoint2;
+    double maxDistance = -1;
+
+    for (int i = 0; i < imagePointsArray[0].size(); i++)
+    {
+        for (int j = i + 1; j < imagePointsArray[0].size(); j++)
+        {
+            double distance = norm(imagePointsArray[0][i] - imagePointsArray[0][j]);
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+                farthestPoint1 = imagePointsArray[0][i];
+                farthestPoint2 = imagePointsArray[0][j];
+            }
+        }
+    }
+
+    circle(undistortedImage, farthestPoint1, 5, Scalar(0, 255, 0), -1);
+    circle(undistortedImage, farthestPoint2, 5, Scalar(0, 255, 0), -1);
+
+    // Display the original and annotated images
+    imshow("Original Image", image);
+    imshow("Annotated Image", undistortedImage);
+    waitKey(0);
+
+    // Process video stream for object detection and distance measurement
+    vector<Point2f> centroids;
+
+    for (;;)
+    {
+        if (!vcap.read(image))
+        {
+            cerr << "No frame" << endl;
+            waitKey();
+            break;
+        }
+
+        process_and_draw_boundaries(image, otsu, centroids, cameraMatrix, distCoeffs);
+
+        if (waitKey(1) >= 0)
+        {
+            break;
+        }
+    }
+
+    // Release video capture and destroy windows
+    vcap.release();
+    destroyAllWindows();
+
+    return 0;
+}
+
+// Function to process the video stream and draw boundaries around detected objects
+void process_and_draw_boundaries(Mat &frame, Mat &threshold_output, vector<Point2f> &centroids,
+                                 const Mat &cameraMatrix, const vector<double> &distCoeffs)
+{
+    // Convert the frame to HSV color space
+    Mat hsv;
+    cvtColor(frame, hsv, COLOR_BGR2HSV);
+
+    // Threshold the frame to detect blue objects
+    Mat blueMask;
+    inRange(hsv, Scalar(100, 50, 50), Scalar(130, 255, 255), blueMask);
+
+    // Equalize the histogram of the saturation channel
+    vector<Mat> channels;
+    split(hsv, channels);
+    equalizeHist(channels[1], channels[1]);
+    merge(channels, hsv);
+
+    // Apply Gaussian blur to the frame
+    Mat blurred;
+    GaussianBlur(hsv, blurred, Size(5, 5), 0);
+
+    // Convert the blurred frame to grayscale
+    Mat gray;
+    cvtColor(blurred, gray, COLOR_BGR2GRAY);
+
+    // Bitwise AND operation to combine gray and blue masks
+    bitwise_and(gray, blueMask, threshold_output);
+
+    // Find contours in the thresholded image
+    vector<vector<Point>> contours;
+    findContours(threshold_output, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    // Sort contours based on area
+    sort(contours.begin(), contours.end(), compareContourAreas);
+
+    // Clear the centroids vector
+    centroids.clear();
+
+    // Draw contours and mark centroids for up to two largest contours
+    for (size_t i = 0; i < min(contours.size(), size_t(2)); ++i)
+    {
+        Moments mu = moments(contours[i]);
+        Point centroid(static_cast<int>(mu.m10 / mu.m00), static_cast<int>(mu.m01 / mu.m00));
+        circle(frame, centroid, 5, Scalar(0, 0, 255), -1);
+
+        centroids.push_back(centroid);
+    }
+
+    drawContours(frame, contours, -1, Scalar(0, 255, 0), 2);
+
+    // If there are at least two centroids, compute and display distance information
+    if (centroids.size() >= 2)
+    {
+        vector<Point2f> imagePoints;
+        imagePoints.push_back(centroids[0]);
+        imagePoints.push_back(centroids[1]);
+
+        vector<Point2f> undistortedPoints;
+        undistortPoints(imagePoints, undistortedPoints, cameraMatrix, distCoeffs);
+
+        double distance_px = norm(centroids[0] - centroids[1]);
+
+        double distance_mm = distance_px * (undistortedPoints[1].x - undistortedPoints[0].x);
+
+        // Display distance information on the frame
+        ostringstream textMm;
+        textMm << "Distance: " << distance_mm << " mm";
+        putText(frame, textMm.str(), Point(10, 60), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 0, 0), 2);
+
+        ostringstream textPx;
+        textPx << "Distance: " << distance_px << " pixels";
+        putText(frame, textPx.str(), Point(10, 90), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 0, 0), 2);
+    }
+
+    // Display the frame with contours and centroids
+    imshow("Video with Contours and Centroid", frame);
+    waitKey(1);
+}
+
+// Function to compute the distance in pixels between corners of a chessboard pattern
+double computeDistancePixels(const vector<Point2f> &corners)
+{
+    double maxDistance = -1;
+    for (int j = 0; j < corners.size(); j++)
+    {
+        for (int k = j + 1; k < corners.size(); k++)
+        {
+            double distance = norm(corners[j] - corners[k]);
+            if (distance > maxDistance)
+            {
+                maxDistance = distance;
+            }
+        }
+    }
+    return maxDistance;
+}
+
+// Function to compare contour areas for sorting
+bool compareContourAreas(vector<Point> contour1, vector<Point> contour2)
+{
+    double i = fabs(contourArea(Mat(contour1)));
+    double j = fabs(contourArea(Mat(contour2)));
+    return (i > j);
+}
+
+// Function to compute the scale factor based on the chessboard pattern
+double computeScaleFactor(const Size &patternSize, const float squareSize, const vector<Point2f> &corners)
+{
+    double distInPixels = norm(corners[0] - corners[patternSize.width]);
+    double distInRealUnits = squareSize;
+    return distInRealUnits / distInPixels;
+}
+
+// Function to compute the distance in real-world units between two points
+double computeDistanceRealUnits(const Point2f &p1, const Point2f &p2, double scaleFactor)
+{
+    double distInPixels = norm(p1 - p2);
+    double distInRealUnits = distInPixels * scaleFactor;
+    return distInRealUnits;
+}
